e7c6c9c7e91b67575bea009dd1ae2f4de853484e
raydin47 <82286974+raydin47@users.noreply.github.com>
2021-07-19 02:28:06 +0300

    Update sayıtahminetme.py

--- sayıtahminetme.py
+++ sayıtahminetme.py
@@ -2,12 +2,12 @@ import random
 for i in range(10):
     x = int(input("Bir sayı gir: "))
     y = int(input("Daha büyük bir sayı gir : "))
-    the_number = random.randint(x, y)
-    guess = int(input( str(x) +" ve " + str(y)+ " arasında bir sayı tahmin et: "))
-    while guess != the_number:
-        if guess > the_number:
+    sayi = random.randint(x, y)
+    tahmin = int(input( str(x) +" ve " + str(y)+ " arasında bir sayı tahmin et: "))
+    while tahmin != sayi:
+        if tahmin > sayi:
             print(guess, "çok yüksekti. Tekrar deneyin")
-        if guess < the_number:
-            print(guess, "çok düşüktü. Tekrar deneyin.")
-        guess = int(input("Tekrar tahmin et: "))
-    print(guess, "numaraydı! Sen kazandın!")
+        if tahmin < the_number:
+            print(tahmin, "çok düşüktü. Tekrar deneyin.")
+        tahmin = int(input("Tekrar tahmin et: "))
+    print(tahmin, "numaraydı! Sen kazandın!")
